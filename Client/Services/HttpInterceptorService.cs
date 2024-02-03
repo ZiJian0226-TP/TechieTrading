a9c35a1cbabd51567ef93dc1ccd3304ff5f71bf2
ZiJian0226-TP <2003980F@student.tp.edu.sg>
2024-02-04 00:08:07 +0800

    Revert "Delete Client directory"
    This reverts commit 6a5c0b3b30cd1a9454c874fbe3dbcf066a8a2580.

---
+++ Client/Services/HttpInterceptorService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Toolbelt.Blazor;
+ 
+namespace TechieTrading.Client.Services
+{
+    public class HttpInterceptorService
+    {
+        private readonly HttpClientInterceptor interceptor;
+        private readonly NavigationManager navManager;
+
+        public HttpInterceptorService(HttpClientInterceptor interceptor, NavigationManager navManager)
+        {
+            this.interceptor = interceptor;
+            this.navManager = navManager;
+        }
+
+        public void MonitorEvent() => interceptor.AfterSend += InterceptResponse;
+
+        private void InterceptResponse(object sender, HttpClientInterceptorEventArgs e)
+        {
+            string message = string.Empty;
+            if (!e.Response.IsSuccessStatusCode)
+            {
+                var responseCode = e.Response.StatusCode;
+
+                switch (responseCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        navManager.NavigateTo("/404");
+                        message = "The requested resource was not found.";
+                        break;
+                    case HttpStatusCode.Unauthorized:
+                    case HttpStatusCode.Forbidden:
+                        navManager.NavigateTo("/unauthorized");
+                        message = "You are not authorized to access this resource. ";
+                        break;
+                    default:
+                        navManager.NavigateTo("/500");
+                        message = "Something went wrong, please contact Administrator";
+                        break;
+                }
+            }
+        }
+
+        public void DisposeEvent() => interceptor.AfterSend -= InterceptResponse;
+    }
+}
+
+
---
+++ Client/Static/Endpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace TechieTrading.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string ProductsEndpoint = $"{Prefix}/products";
+        public static readonly string SellOrdersEndpoint = $"{Prefix}/sellorders";
+        public static readonly string SellOrderItemsEndpoint = $"{Prefix}/sellorderitems";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+        public static readonly string TradeOrdersEndpoint = $"{Prefix}/tradeorders";
+        public static readonly string TradeOrderItemsEndpoint = $"{Prefix}/tradeorderitems";
+    }
+}
